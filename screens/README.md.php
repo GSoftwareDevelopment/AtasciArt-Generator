6177a08c3db4f2a73853965fabc7c33aa60b23df
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-13 11:48:09 +0100

    create description for folder

---
+++ screens/README.md.php
@@ -0,0 +1,8 @@
+Katalog z przykładowymi ekranami oraz plikami konfiguracyjnymi.
+
+W podkatalogach znajdują się:
+- 'atrview' - pliki ekranów do otwarcia w programie Atari Font Maker
+- 'bin' - wersje binarner ekranów
+- 'hex' - pliki tekstowe z zapisem szesnastkowym ekranów
+
+Wszystkie zamieszczone pliki są mojego autorstwa i można je swobodnie modyfikować i/lub wykorzystywać.
