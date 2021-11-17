54b78e7a3acaee02615b96fd70ef151328427b30
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-17 16:32:12 +0100

    update example

--- example.php
+++ example.php
@@ -2,14 +2,13 @@
 
 include('./class_HSCAtasciGen.php');
 
-// expand class
+// extend class
 class HSCGenerator extends AtasciGen {
 	private $gameId;
 
 	public $scoreboard=[];
 
-	public function loadConfig(string $fn, int $gameId = null) {
-		// PL: można tak zrobić, gdyż zakres zmiennyh parametrów funkcji jest inny niz zakres zminnych klasy ($this!)
+	public function __construct($fn, $gameId = null) {
 		if ($gameId===null) throw new Exception("GameID must be defined!");
 		// przed pobraniem danych z bazy, sprawdź obecność $gameId
 		$this->$gameId=$gameId;
@@ -26,14 +25,12 @@ class HSCGenerator extends AtasciGen {
 		["date"=>0, "nick"=>"", "score"=>""],
 		["date"=>0, "nick"=>"", "score"=>""]
 	);
-		// wywołanie nadrzędnej metody wczytania szablonu
-		parent::loadConfig($fn);
+		// HINT: wywołanie nadrzędnej metody (z klasy AtasciGen) wczytania szablonu
+		parent::__construct($fn);
 	}
 
 	function getScoreboardEntry($place) {
 		// This is where the scoreboard data is retrieved :)
-		// skoro pobrałes dane w loadConfig, można przenieść dane
-		// dotyczące wyniky z tablicy wyników do tablicy asocjacyjnej.
 		return [
 			"place"=>$place,
 			"nick"=>$this->scoreboard[$place-1]["nick"],
@@ -41,12 +38,12 @@ class HSCGenerator extends AtasciGen {
 		];
 	}
 }
+
 //
 // example
 //
 
-$gen=new HSCGenerator();
-$gen->loadConfig('screens/kret.json',109); // 109 is game_id
+$gen=new HSCGenerator('screens/kret.json',109); // 109 is game_id
 echo $gen->generate();
 $gen->makeXEX('out.xex',0xbc40);
 
