680a24f70530c9e259d36de0f1b4bdfd4c76009f
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-19 17:12:05 +0100

    make external file with constants for general class

---
+++ _constants.php
@@ -0,0 +1,36 @@
+<?
+const HEX_CHARS="0123456789ABCDEF";
+
+const CONFIG_ELEMENTSCHAMES="elementSchemes";
+const CONFIG_LAYOUTS="layouts";
+const CONFIG_LAYOUTS_ELEMENTS="elements";
+const CONFIG_LAYOUTS_SCREENDATA="screenData";
+const CONFIG_LAYOUTS_ENCODEELEMENTAS="encodeElementsAs";
+const CONFIG_SCREENFILL="screenFill";
+
+const ELEMENT_PLACE="place";
+const ELEMENT_NICK="nick";
+const ELEMENT_SCORE="score";
+const ELEMENT_DATE="date";
+const ELEMENT_TEXT="text";
+const LABEL_SEPARATOR=".";
+
+const ATTR_X="x";
+const ATTR_Y="y";
+const ATTR_XOFFSET="shift";
+const ATTR_WIDTH="width";
+const ATTR_HEIGHT="height";
+const ATTR_USESCHEMA="useSchema";
+const ATTR_ALIGN="align";
+const ATTR_FILLCHAR="fillChar";
+const ATTR_INVERS="inversLine";
+const ATTR_LETTERCASE="letterCase";
+const ATTR_LIMITCHAR="limitChar";
+const ATTR_REPLACEOUTSIDECHAR="replaceOutsideChar";
+const ATTR_SHOWSCOREAS="showScoreAs";
+const ATTR_PRECISION="precision";
+const ATTR_TIMEFORMAT="timeFormat";
+const DEFAULT_TIMEFORMAT="m:s";
+const ATTR_DATEFORMAT="dateFormat";
+const DEFAULT_DATEFORMAT="Y.m.d";
+?>
