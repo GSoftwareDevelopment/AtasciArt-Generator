8cccb1dcb60c3e7d3e78158d0963146682245db9
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-19 17:23:25 +0100

    example of AtasciGen class implementation; extending capabilities by defining multiple layouts in a single configuration file; generating based on game ID and selected layout, from a dedicated file for the game; using default template if no dedicated template is available.

---
+++ class_HSCGenerator.php
@@ -0,0 +1,87 @@
+<?
+include('./class_AtasciGen.php');    // general AtasciGen class - required
+
+class HSCGenerator extends AtasciGen {
+	//
+	// path constants
+
+	const USER_CONFIG_PATH="users_configs/";
+	const DEFAULT_CONFIG_PATH="default_configs/";
+	const DEFAULT_CONFIG_FILE="default_0";
+	const CONFIG_FILE_EXTENTION=".json";
+	const CONFIG_LAYOUTS_DEFAULT="default";
+
+	private $gameID=null;
+	private $layoutID=null;
+
+	public $scoreboard=[];
+
+	public function __construct($gameID = null, $layoutID = self::CONFIG_LAYOUTS_DEFAULT) {
+		if ($gameID===null) throw new Exception("GameID must be defined!");
+
+		$this->gameID=$gameID;
+		$this->layoutID=$layoutID;
+
+		// choice configure file to load (between dedicated file and default)
+		$configFile=
+			self::USER_CONFIG_PATH.
+			$this->gameID.
+			self::CONFIG_FILE_EXTENTION;
+		// check game config file is exist
+		if ( !file_exists($configFile) ) {
+			// if not, check default config file is exist
+			$configFile=
+				self::DEFAULT_CONFIG_PATH.
+				self::DEFAULT_CONFIG_FILE.
+				self::CONFIG_FILE_EXTENTION;
+			if ( !file_exists($configFile) ) {
+				// if default file is not exist, throw exception
+				throw new Exception("Default config file not exist!");
+			}
+			$this->layoutID=self::CONFIG_LAYOUTS_DEFAULT;
+		}
+
+		$this->fetchScoreboardFromDB();
+		// HINT: wywołanie nadrzędnej metody (z klasy AtasciGen) wczytania szablonu
+		parent::__construct($configFile);
+	}
+
+	function fetchScoreboardFromDB() {
+		// przed pobraniem danych z bazy, sprawdź obecność identyfikatora gry `$this->gameId`
+		// tu pobierasz dane z bazy do zminnej $scoreboard
+		$this->scoreboard=array(
+			["date"=>0, "nick"=>"PeBe", "score"=>12345],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""],
+			["date"=>0, "nick"=>"", "score"=>""]
+		);
+	}
+
+	function getScoreboardEntry($place) {
+		// This is where the scoreboard data is retrieved :)
+		return [
+			"place"=>$place,
+			"nick"=>$this->scoreboard[$place-1]["nick"],
+			"score"=>$this->scoreboard[$place-1]["score"]
+		];
+	}
+
+	function generate() {
+		if ($this->layoutID!==null) {
+			if ( isset($this->layoutData[$this->layoutID]) ) {
+				$this->layoutData=&$this->layoutData[$this->layoutID];
+			} else {
+				throw new Exception("The requested definition is not present.");
+			}
+		}
+
+		return parent::generate();
+	}
+}
+?>
