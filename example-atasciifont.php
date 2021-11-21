65d4af6107b94aece823b9f09376943b8df8039c
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-21 17:45:03 +0100

    insert required constants

--- example-atasciifont.php
+++ example-atasciifont.php
@@ -1,5 +1,10 @@
 <?
-include('./class-AtasciiFont.php');
+// defaults for image generator
+const DEFAULT_FONT_FILE="./atari_16.png";
+const DEFAULT_CHAR_WIDTH=16;
+const DEFAULT_CHAR_HEIGHT=16;
+
+include('./class_AtasciiFont.php');
 
 //
 // Run
