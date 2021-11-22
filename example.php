e6ba504fbf286eda9b668bdbbf1000d995ea4732
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-22 09:42:56 +0100

    adding description how to use parameters

--- example.php
+++ example.php
@@ -10,13 +10,23 @@ try {
 // for default layout of dedicated config file.
 // If the game has no dedicated configuration file
 // default configuration file is specified in HSCGenerator::DEFAULT_CONFIG_FILE
-	$gen=new HSCGenerator(109);
+	$gen=new HSCGenerator("1");
 
 // for specified layout (name 'game') of dedicated config file
 //	$gen=new HSCGenerator(109,'game');
 
 // like above, but specified layout has name '1'
 //	$gen=new HSCGenerator(109,'1');
+
+// add parameters
+	$gen->params[]="ATARI";
+	$gen->params[]="RULEZ";
+	$gen->params[]="AND BASTA";
+// The above parameters can be used in a text element.
+// The parameter identifier must be provided in the 'content' attribute,
+// preceded by a percent sign, e.g.
+// content:"%1"
+// content:"%name"
 
 // generate screen - its needed for make image process
 	$gen->generate();
