431b11b2aefda26c5413436ba7958c0d9ac1bfbf
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-11-24 17:38:53 +0100

    function for merge associate arrays

--- _polyfill.php
+++ _polyfill.php
@@ -22,4 +22,18 @@
 			return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
 		}
 	}
-?>
+
+	function array_merge_recursive_distinct(array &$array1, array &$array2)
+	{
+			$merged = $array1;
+
+			foreach ($array2 as $key => &$value) {
+					if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+							$merged[$key] = array_merge_recursive_distinct($merged[$key], $value);
+					} else {
+							$merged[$key] = $value;
+					}
+			}
+
+			return $merged;
+	}?>
